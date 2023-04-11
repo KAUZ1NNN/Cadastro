bad636857c61cbe24f4a90314af20c0f60c7e849
KAUZ1NNN <98913894+KAUZ1NNN@users.noreply.github.com>
2023-04-11 13:12:24 -0300

    Add files via upload

---
+++ WcfService/ClienteService.svc.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using Controller.Repositórios;
+using Entidades;
+using Interfacess;
+
+namespace WcfService
+{
+    // OBSERVAÇÃO: Você pode usar o comando "Renomear" no menu "Refatorar" para alterar o nome da classe "Service1" no arquivo de código, svc e configuração ao mesmo tempo.
+    // OBSERVAÇÃO: Para iniciar o cliente de teste do WCF para testar esse serviço, selecione Service1.svc ou Service1.svc.cs no Gerenciador de Soluções e inicie a depuração.
+    public class ClienteService : ICliente
+    {
+        public RepCad rep = new RepCad();
+        
+        public void Cadastrar(Cliente obj)
+        {
+            rep.Cadastrar(obj);
+        }
+        public Cliente Buscar(int id)
+        {
+           return rep.Buscar(id);
+        }
+        public List<Cliente> Listar()
+        {
+            return rep.Listar();
+        }
+        public void Deletar(int id)
+        {
+            rep = new RepCad();
+        }
+        public void Editar(Cliente objNovo)
+        {
+            rep.Editar(objNovo);
+        }
+
+        public void Cadastrar(ICliente obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
