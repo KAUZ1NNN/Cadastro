ea5bd4f66db71a01a49144e6d117cc30d491f81f
KAUZ1NNN <98913894+KAUZ1NNN@users.noreply.github.com>
2023-04-11 13:11:13 -0300

    Add files via upload

---
+++ Entidades/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public long CPF { get; set; } 
+        public long RG { get; set; }
+        public string Endereco { get; set; }
+        public int N_Endereco { get; set; } 
+        public string Bairro { get; set; }
+        public long CEP { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+    
+}
+}
---
+++ Interfacess/ICliente.cs
@@ -0,0 +1,29 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfacess
+{
+    [ServiceContract]
+    public interface ICliente
+    {
+        [OperationContract]
+        void Cadastrar(ICliente obj);
+
+        [OperationContract]
+        Cliente Buscar(int id);
+
+        [OperationContract]
+        List<Cliente> Listar();
+
+        [OperationContract]
+        void Deletar(int id);
+
+        [OperationContract]
+        void Editar(Cliente objNovo);
+    }
+}
